b2358c90291a4fb66e0c1909a94fe9e3e88b8c15
NTH19 <3310288189@qq.com>
2022-09-26 20:20:54 +0800

    m

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -1,22 +1,5 @@
 package raft
 
-//
-// this is an outline of the API that raft must expose to
-// the service (or tester). see comments below for
-// each of these functions for more details.
-//
-// rf = Make(...)
-//   create a new Raft server.
-// rf.Start(command interface{}) (index, term, isleader)
-//   start agreement on a new log entry
-// rf.GetState() (term, isLeader)
-//   ask a Raft for its current term, and whether it thinks it is leader
-// ApplyMsg
-//   each time a new entry is committed to the log, each Raft peer
-//   should send an ApplyMsg to the service (or tester)
-//   in the same server.
-//
-
 import (
 	//	"bytes"
 	"bytes"
@@ -152,17 +135,20 @@ const (
 //
 // A Go object implementing a single Raft peer.
 //
+
 type Raft struct {
 	mu        sync.Mutex          // Lock to protect shared access to this peer's state
 	peers     []*labrpc.ClientEnd // RPC end points of all peers
 	persister *Persister          // Object to hold this peer's persisted state
 	me        int                 // this peer's index into peers[]
 	dead      int32               // set by Kill()
-	size      int
+
+	size int
 
 	// Your data here (2A, 2B, 2C).
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
+
 	currentTerm int
 	voteFor     int
 	logs        []LogEntry
@@ -1189,5 +1175,6 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	go rf.replicateLoop()
 	go rf.applyLoop()
 
+	//Debug("========"+RAFT_FORMAT+"STARTED========", rf.me)
 	return rf
 }
---
+++ src/raft/simple_test.go
@@ -0,0 +1,11 @@
+package raft
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWhat(t *testing.T) {
+	s := []int{1, 2, 3}
+	fmt.Println(s[:0])
+}
